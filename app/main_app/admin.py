b09e4fcc9539a08a122215aec14d3f3370be99e6
Maxime <allarassemmaxwell@gmail.com>
2024-11-25 00:27:32 +0300

    Implementing the CI including pylint and django test

--- app/main_app/admin.py
+++ app/main_app/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+"""
+Admin configuration for the main app models.
+This module registers models with the Django admin site and customizes their
+presentation and behavior within the admin interface.
+"""
+
+# from django.contrib import admin
 
 # Register your models here.
--- app/main_app/apps.py
+++ app/main_app/apps.py
@@ -1,6 +1,19 @@
+"""
+Configuration for the main app.
+
+This module contains the application configuration for the `main_app` Django app,
+including the custom AppConfig class that specifies settings related to the app.
+"""
+
 from django.apps import AppConfig
 
 
 class MainAppConfig(AppConfig):
+    """
+    Configuration for the MainApp Django application.
+
+    This class defines settings specific to the `main_app` application, including
+    setting the default auto field and importing signals when the app is ready.
+    """
     default_auto_field = "django.db.models.BigAutoField"
     name = "main_app"
