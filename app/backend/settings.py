35f01a651ef0319030ab1bf9eeaab1f79c005658
Allarassem Maxime <allarassemmaxwell@users.noreply.github.com>
2024-11-27 00:25:55 +0300

    Update settings.py

--- app/backend/settings.py
+++ app/backend/settings.py
@@ -121,6 +121,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+# settings.py
+SECURE_SSL_REDIRECT = False  # Prevent Django from redirecting HTTP to HTTPS
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
